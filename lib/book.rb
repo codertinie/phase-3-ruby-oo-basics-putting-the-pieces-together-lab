542138408b643f34b13c1c05eb78f8b1a1238b88
codertinie <martin.munyao@moringaschool.com>
2022-10-20 18:20:49 +0300

    commit tree b965ac4f58348ac2113a5af1ee6c1a8b8ea0bd6f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
+
+  def initialize(title)
+    @title = title
+  end
+
+  def properties=(title, author, page_count)
+    @title = title
+    @author = author
+    @page_count = page_count
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
